0609ed8831ed63a35671cfa14ef39e64d33cb8fc
valentino mastantuono <valentinomastantuono@gmail.com>
2022-06-01 19:07:15 -0300

    entregable 1

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+//                                 CONSIGNA 1
+/*const numeroIngresado = Number(prompt('Ingresar un Numero'))
+for (let i = 0; i<=20;i++){
+    console.log(i + numeroIngresado)
+} */
+
+//                                 CONSIGNA 2
+/*numeroIngresado = Number(prompt('Ingresar un Numero'))
+
+while (numeroIngresado <=67) {
+    console.log(numeroIngresado)
+    numeroIngresado += 2
+}*/
+
+//                                 CONSIGNA 3
+/*let password
+do {
+    password = Number(prompt('ingrese su contraseña'))
+}   while (password !== 1234567)
+alert( 'Bienvenido a la clase de JS! :D')*/
